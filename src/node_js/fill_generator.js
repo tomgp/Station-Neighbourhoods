e137a28362779046027bfb2b7463ed404f426e4a
Tom <tom.g.pearson@gmail.com>
2012-11-01 16:32:41 +0000

    added extra faded colours, and test page for visually checking the robustness of the tiling for fills generated

--- src/node_js/fill_generator.js
+++ src/node_js/fill_generator.js
@@ -3,27 +3,57 @@ var fs = require('fs');
 
 function drawFill(colours, strip_width, file){
 	//create a canavs based on how many stripes we need
-	canavs_dim = strip_width * colours.length
+	canvas_dim = strip_width * colours.length
 	
 	var first = true;
-	var canvas = new Canvas(canavs_dim, canavs_dim);
+	var canvas = new Canvas(canvas_dim, canvas_dim);
 	var ctx = canvas.getContext('2d');
 	//draw the first half of the stripes
-	for (var i in colours){
-		if(first){
-			ctx.fillStyle = colours[i];
-			ctx.fillRect(0,0,canavs_dim,canavs_dim);
-			first = false;
-		}else{
-			//start width * i down 
-		}
+	for (var i = 0;i < colours.length; i++){
+		i = Number(i);
+		var points = [
+			{x:0, y:(strip_width * i)},
+			{x:0, y:strip_width * (i+1)},
+			{x:strip_width * (i+1),y:0},
+			{x:strip_width * i,y:0}];
+		drawShape(ctx, points,  colours[i].fill,  colours[i].stroke);
 	}
+	for (var i = 0;i < colours.length; i++){
+		i = Number(i);
+		var points = [
+			{x:(strip_width * i), y:canvas_dim},
+			{x:strip_width * (i+1), y:canvas_dim},
+			{x:canvas_dim, y:strip_width * (i+1)},
+			{x:canvas_dim, y:strip_width * i}];
+		drawShape(ctx, points,  colours[i].fill,  colours[i].stroke);
+	}
+
 	var out = fs.createWriteStream(file);
   	var stream = canvas.createPNGStream();
 
 	stream.on('data', function(chunk){
-	  out.write(chunk);
+		console.log('writing chunk');
+		out.write(chunk);
 	});
 }
 
-drawFill(['#ff0000','#00ff00'], 20, "file_name.png");
+function drawShape(context, points, fill, stroke){
+	console.log("drawing shape with " + points.length + " points");
+	console.log(points);
+	context.beginPath();
+	context.fillStyle = fill;
+	context.strokeStyle = stroke;
+	//move to the last point
+	context.moveTo(points[points.length-1].x, points[points.length-1].y );
+	for(var i = 0; i<points.length; i++){
+		console.log('line ' + points[i].x +", " + points[i].y);
+		context.lineTo(points[i].x, points[i].y);
+	}
+	context.stroke();
+	context.fill();
+}
+
+var c = [{fill:'#aaa',stroke:'#aaa'},
+		{fill:'#000',stroke:'#000'}];
+
+drawFill(c, 20, "test/tile.png");
